6a64c22b1b7639396f3ab8537c36097f5ea1a64f
Ruhan Borgmann <ruhandb@gmail.com>
2024-02-13 12:54:29 +0000

    assetPrefix

--- token-tide-ui/next.config.mjs
+++ token-tide-ui/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: './token-tide'
+    assetPrefix: '/token-tide'
 };
 
 export default nextConfig;
